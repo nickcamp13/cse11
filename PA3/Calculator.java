33cca3dce79e67e4684a4e3d07ba1d4024ad73e0
Nick Campos <nickreyescampos@gmail.com>
2023-04-26 18:12:46 -0700

    refactored code and added new helper methods for styling purposes

--- PA3/Calculator.java
+++ PA3/Calculator.java
@@ -21,10 +21,107 @@
 public class Calculator {
     private static final char DECIMAL_POINT = '.';
     private static final char ZERO = '0';
+    private static final String CARRY_ONE = "1";
     private static final String ADD_MISSING_DECIMAL = ".0";
     private static final int CARRY_CUTOFF = 9;
     private static final int CARRY_SUBTRACT = 10;
     private static final int VALID_CHARACTER = 2;
+
+    /**
+     * Finds the index of a numbers decimal point
+     * 
+     * @param number the number to search for its
+     *               decimal point
+     * @return the index of numbers decimal point
+     */
+    private static int indexOfDecimal(String number) {
+        return number.indexOf(DECIMAL_POINT);
+    }
+
+    /**
+     * Adds a decimal point to the end of a number
+     * 
+     * @param number the number of which a decimal point will
+     *               be added to
+     * @return the number passed but with the added decimal
+     *         appended to it
+     */
+    private static String addDecimalPoint(String number) {
+        return number += ADD_MISSING_DECIMAL;
+    }
+
+    /**
+     * Checks if number contains a decimal
+     * 
+     * @param number the number of which a decimal will be
+     *               searched for
+     * @return true if number has decimal, false otherwise
+     */
+    private static boolean hasDecimalPoint(String number) {
+        return number.contains(Character.toString(DECIMAL_POINT));
+    }
+
+    /**
+     * Checks if number starts with a decimal (has no whole
+     * number value).
+     * 
+     * @param number the number of which to see if it begins
+     *               with a decimal
+     * @return true if number starts with a decimal, false
+     *         otherwise
+     */
+    private static boolean startsWithDecimalPoint(String number) {
+        return number.charAt(0) == DECIMAL_POINT;
+    }
+
+    /**
+     * Checks if a number starts with a zero
+     * 
+     * @param number the number of which to see if it begins
+     *               with a zero
+     * @return true if number starts with zero, false otherwise
+     */
+    private static boolean startsWithZero(String number) {
+        return number.charAt(0) == ZERO;
+    }
+
+    /**
+     * Checks if number ends with a decimal
+     * 
+     * @param number the number of which to check if it ends
+     *               with a decimal
+     * @return true if number ends with a decimal, false otherwise
+     */
+    private static boolean endsWithDecimalPoint(String number) {
+        return number.charAt(number.length() - 1) == DECIMAL_POINT;
+    }
+
+    /**
+     * Checks if a number ends with a zero
+     * 
+     * @param number the number to check if it ends
+     *               with a zero
+     * @return true if ends with zero, false otherwise
+     */
+    private static boolean endsWithZero(String number) {
+        return number.charAt(number.length() - 1) == ZERO;
+    }
+
+    /**
+     * Calculates the sum of single digit numbers
+     * represented as character typed values
+     * 
+     * @param firstDigit  the first digit in the equation
+     * @param secondDigit the second digit in the equation
+     * @return the integer value of the sum of the two numbers
+     */
+    private static int getSum(char firstDigit, char secondDigit) {
+        // subtracts the int value of char '0' to get the int value
+        // of the char digit
+        int firstDigitInt = firstDigit - ZERO;
+        int secondDigitInt = secondDigit - ZERO;
+        return firstDigitInt + secondDigitInt;
+    }
 
     /**
      * Retrieves the whole number value of number
@@ -38,14 +135,14 @@ public class Calculator {
         String wholeNumber = "";
 
         // should return empty string if no whole number
-        if (number.charAt(0) == DECIMAL_POINT) {
+        if (startsWithDecimalPoint(wholeNumber)) {
             return wholeNumber;
         }
 
         // a number containing a decimal means everything to the left
         // of it is the whole number
-        if (number.contains(Character.toString(DECIMAL_POINT))) {
-            for (int i = 0; i < number.indexOf(DECIMAL_POINT); ++i) {
+        if (hasDecimalPoint(wholeNumber)) {
+            for (int i = 0; i < indexOfDecimal(number); ++i) {
                 wholeNumber += number.charAt(i);
             }
         } else {
@@ -68,8 +165,8 @@ public class Calculator {
 
         // everything to the right of a decimal is the decimal
         // portion of the number
-        if (number.contains(Character.toString(DECIMAL_POINT))) {
-            for (int i = number.length() - 1; i > number.indexOf(DECIMAL_POINT); --i) {
+        if (hasDecimalPoint(number)) {
+            for (int i = number.length() - 1; i > indexOfDecimal(number); --i) {
                 decimal = number.charAt(i) + decimal;
             }
         }
@@ -117,53 +214,6 @@ public class Calculator {
     }
 
     /**
-     * Adds a decimal point to the end of a number
-     * 
-     * @param number the number of which a decimal point will
-     *               be added to
-     * @return the number passed but with the added decimal
-     *         appended to it
-     */
-    private static String addDecimalPoint(String number) {
-        return number += ADD_MISSING_DECIMAL;
-    }
-
-    /**
-     * Checks if number contains a decimal
-     * 
-     * @param number the number of which a decimal will be
-     *               searched for
-     * @return true if number has decimal, false otherwise
-     */
-    private static boolean hasDecimalPoint(String number) {
-        return number.contains(Character.toString(DECIMAL_POINT));
-    }
-
-    /**
-     * Checks if number starts with a decimal (has no whole
-     * number value).
-     * 
-     * @param number the number of which to see if it begins
-     *               with a decimal
-     * @return true if number starts with a decimal, false
-     *         otherwise
-     */
-    private static boolean startsWithDecimalPoint(String number) {
-        return number.charAt(0) == DECIMAL_POINT;
-    }
-
-    /**
-     * Checks if number ends with a decimal
-     * 
-     * @param number the number of which to check if it ends
-     *               with a decimal
-     * @return true if number ends with a decimal, false otherwise
-     */
-    private static boolean endsWithDecimalPoint(String number) {
-        return number.charAt(number.length() - 1) == DECIMAL_POINT;
-    }
-
-    /**
      * Formats number by removing all trailing and leading zeros
      * from a number. It also ensures that a number contains a
      * decimal and digits exist before and after it.
@@ -183,11 +233,11 @@ public class Calculator {
 
         // need to keep track of the original value since the string
         // shortens each time substring is used
-        int originalIndexOfDecimal = number.indexOf(DECIMAL_POINT);
+        int originalIndexOfDecimal = indexOfDecimal(number);
         for (int i = 0; i < originalIndexOfDecimal; ++i) {
             // remove leading zeros unless it is the only 0 before the decimal
             if (!startsWithDecimalPoint(number) &&
-                    (i + 1 != originalIndexOfDecimal && number.charAt(0) == ZERO)) {
+                (i + 1 != originalIndexOfDecimal && startsWithZero(number))) {
                 // removes the current leading 0
                 number = number.substring(1, number.length());
                 continue;
@@ -198,33 +248,17 @@ public class Calculator {
         }
 
         // get new "original" index of decimal after removing leading zeros
-        originalIndexOfDecimal = number.indexOf(DECIMAL_POINT);
+        originalIndexOfDecimal = indexOfDecimal(number);
         for (int i = number.length() - 1; i > originalIndexOfDecimal; --i) {
-            if (number.charAt(number.length() - 1) == ZERO
-                    && (number.length() - VALID_CHARACTER != originalIndexOfDecimal
-                            || number.charAt(number.length() - VALID_CHARACTER) == ZERO)) {
+            if (endsWithZero(number)
+                && (number.length() - VALID_CHARACTER != originalIndexOfDecimal
+                || number.charAt(number.length() - VALID_CHARACTER) == ZERO)) {
                 number = number.substring(0, number.length() - 1);
                 continue;
             }
             break;
         }
         return number;
-    }
-
-    /**
-     * Calculates the sum of single digit numbers
-     * represented as character typed values
-     * 
-     * @param firstDigit  the first digit in the equation
-     * @param secondDigit the second digit in the equation
-     * @return the integer value of the sum of the two numbers
-     */
-    private static int getSum(char firstDigit, char secondDigit) {
-        // subtracts the int value of char '0' to get the int value
-        // of the char digit
-        int firstDigitInt = firstDigit - ZERO;
-        int secondDigitInt = secondDigit - ZERO;
-        return firstDigitInt + secondDigitInt;
     }
 
     /**
@@ -239,16 +273,16 @@ public class Calculator {
      * @return the result of adding two single digit numbers
      *         with a potential carry factor
      */
-    public static char addDigits(char firstDigit, char secondDigit, boolean carryIn) {
+    public static char addDigits
+    (char firstDigit, char secondDigit, boolean carryIn) {
         int sum = getSum(firstDigit, secondDigit);
+        if (carryIn) {
+            ++sum;
+        }
         if (sum > CARRY_CUTOFF) {
-            // subtract 10 to get the rightmost digit
             sum -= CARRY_SUBTRACT;
         }
         char sumChar = (char) ((int) ZERO + sum);
-        if (carryIn) {
-            ++sumChar;
-        }
         return sumChar;
     }
 
@@ -264,7 +298,8 @@ public class Calculator {
      * @return true if the sum is greater than the carry cutoff,
      *         false otherwise
      */
-    public static boolean carryOut(char firstDigit, char secondDigit, boolean carryIn) {
+    public static boolean carryOut
+    (char firstDigit, char secondDigit, boolean carryIn) {
         if (carryIn) {
             return getSum(firstDigit, secondDigit) + 1 > CARRY_CUTOFF;
         }
@@ -287,35 +322,37 @@ public class Calculator {
 
         // important to keep track of how many digits are on both
         // sides of each number
-        int numDigitsBeforeDecimal1 = 0;
-        int numDigitsBeforeDecimal2 = 0;
-        int numDigitsAfterDecimal1 = 0;
-        int numDigitsAfterDecimal2 = 0;
+        int digitsBeforeDecimal1 = 0;
+        int digitsBeforeDecimal2 = 0;
+        int digitsAfterDecimal1 = 0;
+        int digitsAfterDecimal2 = 0;
 
-        for (int i = 0; i < firstNumber.indexOf(DECIMAL_POINT); ++i) {
-            numDigitsBeforeDecimal1++;
+        for (int i = 0; i < indexOfDecimal(firstNumber); ++i) {
+            digitsBeforeDecimal1++;
         }
-        for (int i = 0; i < secondNumber.indexOf(DECIMAL_POINT); ++i) {
-            numDigitsBeforeDecimal2++;
+        for (int i = 0; i < indexOfDecimal(secondNumber); ++i) {
+            digitsBeforeDecimal2++;
         }
 
         // this variable determines how to align both numbers
-        int differenceInDigits = numDigitsBeforeDecimal2 - numDigitsBeforeDecimal1;
+        int differenceInDigits = digitsBeforeDecimal2 - digitsBeforeDecimal1;
         if (differenceInDigits > 0) {
             firstNumber = prependZeros(firstNumber, differenceInDigits);
         } else if (differenceInDigits < 0) {
             secondNumber = prependZeros(secondNumber, differenceInDigits * -1);
         }
 
-        for (int i = firstNumber.length() - 1; i > firstNumber.indexOf(DECIMAL_POINT); --i) {
-            numDigitsAfterDecimal1++;
+        for (int i = firstNumber.length() - 1;
+            i > indexOfDecimal(firstNumber); --i) {
+            digitsAfterDecimal1++;
         }
-        for (int i = secondNumber.length() - 1; i > secondNumber.indexOf(DECIMAL_POINT); --i) {
-            numDigitsAfterDecimal2++;
+        for (int i = secondNumber.length() - 1;
+            i > indexOfDecimal(secondNumber); --i) {
+            digitsAfterDecimal2++;
         }
 
         // needs to be recalculated for digits after the decimal
-        differenceInDigits = numDigitsAfterDecimal2 - numDigitsAfterDecimal1;
+        differenceInDigits = digitsAfterDecimal2 - digitsAfterDecimal1;
         if (differenceInDigits > 0) {
             firstNumber = appendZeros(firstNumber, differenceInDigits);
         } else if (differenceInDigits < 0) {
@@ -337,6 +374,9 @@ public class Calculator {
             result = addDigits(charFirstNum, charSecondNum, carryIn) + result;
             if (carryOut(charFirstNum, charSecondNum, carryIn)) {
                 carryIn = true;
+                if (i == 0) {
+                    result = CARRY_ONE + result;
+                }
                 continue;
             }
             carryIn = false;
