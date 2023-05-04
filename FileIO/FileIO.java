d3b1fd8a3181d7b6db11ec1e12764cb9ee1cc064
Nick Campos <nickreyescampos@gmail.com>
2023-05-03 18:54:13 -0700

    PrintWriter and Scanner

--- FileIO/FileIO.java
+++ FileIO/FileIO.java
@@ -1,22 +1,44 @@
 import java.io.IOException;
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileIO {
     public static void main(String[] args) throws IOException {
+        
+        /**
+         * PrintWriter Example
+         */
+        // File file = new File("scores.txt");
+        // if (file.exists()) {
+        //     System.out.println("File already exists");
+        //     System.exit(1);
+        // }
+
+        // try (
+        //     PrintWriter output = new PrintWriter(file);
+        // ) {
+        //     output.print("John T Smith ");
+        //     output.println(90);
+        //     output.print("Jared T Jones ");
+        //     output.println(85);
+        // }
+
+        /**
+         * Scanner Example
+         */
         File file = new File("scores.txt");
-        if (file.exists()) {
-            System.out.println("File already exists");
-            System.exit(1);
+        Scanner input = new Scanner(file);
+
+        while (input.hasNext()) {
+            String firstName = input.next();
+            String mi = input.next();
+            String lastName = input.next();
+            int score = input.nextInt();
+            System.out.println(
+                firstName + " " + mi + " " + lastName + " " + score
+                );
         }
-
-        PrintWriter output = new PrintWriter(file);
-
-        output.print("John T Smith");
-        output.println(90);
-        output.print("Jared T Jones");
-        output.println(85);
-
-        output.close();
+        input.close();
     }
 }
